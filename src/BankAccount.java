9068c31c4636e16c4665eb5d80cef1e2a0c14306
Paul Franklin <paul.franklin@csuglobal.edu>
2024-03-20 19:29:49 -0400

    First save of BankAccount, building methods and attributes

---
+++ src/BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+    String firstName;
+    String lastName;
+    int accountID;
+    double balance;
+    public BankAccount(){}
+    public void constructor(){}
+    public void deposit (double deposit){}
+    public void withdrawal (double withdrawal){}
+    public double getBalance(){
+        return this.balance;}
+    public void accountSummary(){
+        System.out.println("FIX_ME");}
