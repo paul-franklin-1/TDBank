32347500d287bf712774f27b8f08896348e94040
Paul Franklin <paul.franklin@csuglobal.edu>
2024-03-20 20:48:31 -0400

    Starting CheckingAccount file

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -8,27 +8,40 @@ public class BankAccount {
         this.balance = 0;
     }
 
-    public void constructor() {
+    public void NOOOOconstructor() {
 
     }
 
     public void deposit(double deposit) {
+        this.balance += deposit;}
+
+    public void withdrawal(double withdrawal) {
+        this.balance -= withdrawal;}
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
     }
-
-    public void withdrawal(double withdrawal) {}
-
-    public void setFirstName(String firstName){}
-    public void setLastName(String lastName){}
-    public void setAccountID(int accountID){}
-    public String getFirstName(){}
-    public String getLastName(){}
-    public int getAccountID(){}
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAccountID(int accountID){
+        this.accountID = accountID;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public int getAccountID(){
+        return this.accountID;
+    }
 
     public double getBalance() {
         return this.balance;
     }
 
     public void accountSummary() {
-        System.out.println("FIX_MEEee");
+        System.out.println("Name: " + this.firstName + " " + this.lastName + "\nAccount ID: " + this.accountID +
+                "\nBalance: " + this.balance);}
     }
 }
--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,4 +1,7 @@
 public class CheckingAccount{
+    public String overdraft(){
+        if
+        return
 
     }
 
