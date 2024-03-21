2736a5af9a1891ac9cf0d0b95407aba862e2ff22
Paul Franklin <paul.franklin@csuglobal.edu>
2024-03-20 20:35:01 -0400

    Second save

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -5,16 +5,24 @@ public class BankAccount {
     double balance;
 
     public BankAccount() {
+        this.balance = 0;
     }
 
     public void constructor() {
+
     }
 
     public void deposit(double deposit) {
     }
 
-    public void withdrawal(double withdrawal) {
-    }
+    public void withdrawal(double withdrawal) {}
+
+    public void setFirstName(String firstName){}
+    public void setLastName(String lastName){}
+    public void setAccountID(int accountID){}
+    public String getFirstName(){}
+    public String getLastName(){}
+    public int getAccountID(){}
 
     public double getBalance() {
         return this.balance;
