c8e1d14985122b9600f15cbd56203290e69deaa0
Paul Franklin <paul.franklin@csuglobal.edu>
2024-03-24 16:55:04 -0400

    Closing in on completion

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,30 @@
 public class Test {
     public static void main(String[] args) {
+        CheckingAccount johnAccount = new CheckingAccount();
+        johnAccount.setFirstName("John");
+        johnAccount.setLastName("Doe");
+        johnAccount.setAccountID(54321);
+        johnAccount.deposit(5000);
+        johnAccount.withdrawal(1000);
+
         CheckingAccount paulAccount = new CheckingAccount();
         paulAccount.setFirstName("Paul");
         paulAccount.setLastName("Franklin");
-        paulAccount.setAccountID(99999);
+        paulAccount.setAccountID(12345);
         paulAccount.deposit(1000);
-        paulAccount.processWithdrawal(3000);
+        paulAccount.withdrawal(300);
+
+        System.out.println("Account information for \"paulAccount\":");
+        paulAccount.getFirstName();
+        paulAccount.getLastName();
         paulAccount.getAccountID();
-        paulAccount.accountSummary();
-}}
+        paulAccount.getBalance();
+
+        System.out.println();
+
+        CheckingAccount emptyAccount = new CheckingAccount();
+        System.out.println("Account information for \"emptyAccount\":");
+        emptyAccount.getBalance();
+
+    }}
+
